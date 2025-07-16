3ce3c8bf6f9e65af6f3cb5d2797f2df02ffda2fb
Thiago F. Martins <thiagofm33@gmail.com>
2025-07-16 11:09:23 -0300

    Updated `lib/presentation/widgets/locations_widget/state_farms.dart`

--- lib/presentation/widgets/locations_widget/state_farms.dart
+++ lib/presentation/widgets/locations_widget/state_farms.dart
@@ -1,5 +1,6 @@
 import 'package:fiap_farms_app/presentation/widgets/locations_widget/percentage_ring.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class StateFarms extends StatelessWidget {
   final String state;
@@ -18,15 +19,22 @@ class StateFarms extends StatelessWidget {
     return Column(
       children: [
         PercentageRing(value: percentage),
+        const SizedBox(height: 3),
         Text(
           state,
           style: TextStyle(
-              fontSize: 18,
-              color: Color(0xFF97133E),
-              fontWeight: FontWeight.bold),
+            fontSize: 18,
+            color: Color(0xFF97133E),
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
         ),
         Text(
-          "$farmsNumber Fazendas",
+          Intl.plural(
+            farmsNumber,
+            one: '1 Fazenda',
+            other: '$farmsNumber Fazendas',
+          ),
           style: TextStyle(
             fontSize: 16,
           ),
