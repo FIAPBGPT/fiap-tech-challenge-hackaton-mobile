067e47e080db14749f793681f20f26f864a16bb1
Thiago F. Martins <thiagofm33@gmail.com>
2025-07-17 10:21:23 -0300

    Updated some repositories

--- lib/domain/repositories/venda_repository.dart
+++ lib/domain/repositories/venda_repository.dart
@@ -4,5 +4,6 @@ abstract class VendaRepository {
   Future<void> addVenda(Venda venda);
   Future<void> updateVenda(Venda venda);
   Future<void> deleteVenda(Venda venda);
+  Future<List<Venda>> getAll();
   Stream<List<Venda>> watchAllVendas();
 }
